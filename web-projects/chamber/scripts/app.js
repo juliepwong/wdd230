23d689f9c79b91fbafe805c4943095af7ffc2907
juliepwong <won20001@byui.edu>
2023-05-27 21:34:12 -0700

    date

--- web-projects/chamber/scripts/app.js
+++ web-projects/chamber/scripts/app.js
@@ -1,2 +1,11 @@
 const date = new Date();
 document.getElementById("date").innerHTML = date;
+
+const meeting = new Date();
+const day2 = meeting.getDay();
+// Sunday - Saturday : 0 - 6
+
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",]
+
+console.log(day2);
+// Day: Tuesday
