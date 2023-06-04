bb7d58c1c2dc93a805d5e2741fa06d21095e14be
juliepwong <won20001@byui.edu>
2023-06-03 22:33:33 -0700

    fixing java script

---
+++ web-projects/lesson5/scripts/app.js
@@ -0,0 +1,8 @@
+const element = document.getElementById("submit");
+element.addEventListener("click", myFunction)
+
+function myFunction() {
+document.getElementById("scripture").innerHTML = "My favorite chapter"; 
+}
+
+let text = document.lastModified; document.getElementById("time").innerHTML = text;
