0d60d93632b7d499ee0565549874c987ee053643
juliepwong <won20001@byui.edu>
2023-06-03 22:35:27 -0700

    updating time link

--- web-projects/lesson5/scripts/app.js
+++ web-projects/lesson5/scripts/app.js
@@ -1,8 +1,8 @@
+let text = document.lastModified; document.getElementById("time").innerHTML = text;
+
 const element = document.getElementById("submit");
 element.addEventListener("click", myFunction)
 
 function myFunction() {
 document.getElementById("scripture").innerHTML = "My favorite chapter"; 
 }
-
-let text = document.lastModified; document.getElementById("time").innerHTML = text;
