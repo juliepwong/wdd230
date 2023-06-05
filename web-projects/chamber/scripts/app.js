3630544e60efe93e67e14899580c728335b7cc34
juliepwong <won20001@byui.edu>
2023-06-04 22:57:56 -0700

    fixing sizes

--- web-projects/chamber/scripts/app.js
+++ web-projects/chamber/scripts/app.js
@@ -1,5 +1,5 @@
-const date = new Date();
-document.getElementById("date").innerHTML = date;
-
 let text = document.lastModified; 
 document.getElementById("time").innerHTML = text;
+
+const date = new Date();
+document.getElementById("date").innerHTML = date;
