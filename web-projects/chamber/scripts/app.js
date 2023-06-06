237919e58e75411b5d904b261ea4e72dae4eeca9
juliepwong <won20001@byui.edu>
2023-06-05 20:06:05 -0700

    fix links

--- web-projects/chamber/scripts/app.js
+++ web-projects/chamber/scripts/app.js
@@ -3,3 +3,13 @@ document.getElementById("time").innerHTML = text;
 
 const date = new Date();
 document.getElementById("date").innerHTML = date;
+
+//if today is monday or tuesday get banner and show grid//
+
+if (date.getDay() === 1 || date.getDay() === 2) {
+    banner = document.getElementById("banner");
+    banner.style.display = "grid";
+}
+
+
+
