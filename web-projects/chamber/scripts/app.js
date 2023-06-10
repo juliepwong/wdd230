0b352e0047ce62b3c8b8af3bc480d6bf44714b2a
juliepwong <won20001@byui.edu>
2023-06-10 13:48:41 -0700

    fixed layout

--- web-projects/chamber/scripts/app.js
+++ web-projects/chamber/scripts/app.js
@@ -2,14 +2,24 @@ let text = document.lastModified;
 document.getElementById("time").innerHTML = text;
 
 const date = new Date();
-document.getElementById("date").innerHTML = date;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById("date").innerHTML = date.toLocaleDateString('en-us', options);
 
 //if today is monday or tuesday get banner and show grid//
 
 if (date.getDay() === 1 || date.getDay() === 2) {
     banner = document.getElementById("banner");
-    banner.style.display = "grid";
+    banner.style.display = "flex";
 }
+
+// responsive hamburger menu functionality
+
+let hamburger = document.querySelector(".hamburger-menu");
+let navigator = document.querySelector("nav");
+
+hamburger.addEventListener('click', () => {
+    navigator.classList.toggle('responsive');
+});
 
 const nav = document.getElementById(nav)
 
