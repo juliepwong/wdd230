fbafcdd39a98edbf80d2e373bf571b3863cae030
juliepwong <won20001@byui.edu>
2023-06-10 21:17:04 -0700

    image size change

--- web-projects/test-site/scripts/app.js
+++ web-projects/test-site/scripts/app.js
@@ -35,3 +35,40 @@ const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 const fulldate = `${monthName} ${d.getDate()}, ${year}`;
 document.querySelector("#date2").textContent = fulldate;
+
+
+
+
+/* discover page */
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+} 
+  
+else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+}
