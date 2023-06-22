5952511c5b89ee3c594c12149ad7bda89242c806
juliepwong <won20001@byui.edu>
2023-06-22 13:51:33 -0700

    fixed banner

--- web-projects/lesson05/scripts/app.js
+++ web-projects/lesson05/scripts/app.js
@@ -8,3 +8,35 @@ document.getElementById("time").innerHTML = text;
 function myFunction() {
 document.getElementById("scripture").innerHTML = document.getElementById("favchap").value; 
 }
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+
+    document.getElementById("myUL").appendChild(li);
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+  
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
