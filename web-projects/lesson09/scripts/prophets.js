4980b6d2476bd3a12e2d00e1d5eb435e2261e40b
juliepwong <won20001@byui.edu>
2023-06-27 18:43:52 -0700

    prophets

--- web-projects/lesson09/scripts/prophets.js
+++ web-projects/lesson09/scripts/prophets.js
@@ -19,6 +19,13 @@ const displayProphets = (prophets) => {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
+    let birthdate = document.createElement('p');
+    let death = document.createElement('p');
+    let length = document.createElement('p');
+    let order = document.createElement('p');
+    let birthplace = document.createElement('p');
+    let numofchildren = document.createElement('p');
+
 
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
 
@@ -28,8 +35,22 @@ const displayProphets = (prophets) => {
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
 
+    // console.log(prophet)
+    birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    numofchildren.textContent = `Children: ${prophet.numofchildren}`;
+    length.textContent = `Prophet Years: ${prophet.length}`;
+    order.textContent = `Propeht Order: ${prophet.order}`;
+    death.textContent = `Date of Death: ${prophet.death}`;
+
     card.appendChild(h2);
     card.appendChild(portrait);
+    card.appendChild(birthdate);
+    card.appendChild(death);
+    card.appendChild(length);
+    card.appendChild(order);
+    card.appendChild(birthplace);
+    card.appendChild(numofchildren);
 
     cards.appendChild(card);
   }) //close of for each loop
