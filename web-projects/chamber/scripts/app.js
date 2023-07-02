f9037d1dd7a6e738af3c82199d5bc6e5588e7d5c
juliepwong <won20001@byui.edu>
2023-07-01 21:48:06 -0700

    directory

--- web-projects/chamber/scripts/app.js
+++ web-projects/chamber/scripts/app.js
@@ -1,3 +1,5 @@
+const url = 'https://juliepwong.github.io/wdd230/web-projects/chamber/directory/data.json';
+
 let text = document.lastModified; 
 document.getElementById("time").innerHTML = text;
 
@@ -20,4 +22,48 @@ let navigator = document.querySelector("nav");
 hamburger.addEventListener('click', () => {
     navigator.classList.toggle('responsive');
 });
+
+
+// directory
+async function getDirectoryData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayDirectoru(data.directory);
+}
+
+getDirectoryData(url)
+
+const displayDirectory = (directory) => {
+  const cards = document.querySelector('div.cards');
+    
+  directory.forEach((directory) => {
+      let image = document.createElement('img');
+      let address = document.createElement('p');
+      let death = document.createElement('p');
+      let length = document.createElement('p');
+      let order = document.createElement('p');
+
+      image.setAttribute('src', image.imageurl)
+      image.setAttribute('alt', `${prophet.name} ${prophet.lastname}`)
+      image.setAttribute('loading', 'lazy');
+      image.setAttribute('width', '340');
+      image.setAttribute('height', '440');
+      
+      
+      birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+      numofchildren.textContent = `Children: ${prophet.numofchildren}`;
+      length.textContent = `Prophet Years: ${prophet.length}`;
+      order.textContent = `Propeht Order: ${prophet.order}`;
+      death.textContent = `Date of Death: ${prophet.death}`;
+      
+      card.appendChild(place);
+      card.appendChild(portrait);
+      card.appendChild(birthdate);
+      card.appendChild(death);
+      card.appendChild(length);
+      
+      )}
+}
+
+
 
