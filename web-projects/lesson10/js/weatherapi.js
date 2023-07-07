c4d767896593e687c1402692c22a45140abd6ccc
juliepwong <won20001@byui.edu>
2023-07-06 20:59:35 -0700

    join

---
+++ web-projects/lesson10/js/weatherapi.js
@@ -0,0 +1,21 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
