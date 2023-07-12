93b34654f73458cb143b6841cc1dec633bf2b752
juliepwong <won20001@byui.edu>
2023-07-11 21:40:30 -0700

    Bountiful Foods

--- web-projects/bountiful-foods/scripts/app.js
+++ web-projects/bountiful-foods/scripts/app.js
@@ -1,5 +1,12 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
 
+let hamburger = document.querySelector(".hamburger-menu");
+let navigator = document.querySelector("nav");
+
+hamburger.addEventListener('click', () => {
+    navigator.classList.toggle('responsive');
+});
+
 let text = document.lastModified; 
 document.getElementById("time").innerHTML = text;
 
