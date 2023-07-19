6698f1c8da65db0d147dbf212cd506edf24c0f86
juliepwong <won20001@byui.edu>
2023-07-18 22:02:36 -0700

    fresh page

--- web-projects/bountiful-foods/scripts/app.js
+++ web-projects/bountiful-foods/scripts/app.js
@@ -13,3 +13,53 @@ document.getElementById("time").innerHTML = text;
 const date = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 document.getElementById("date").innerHTML = date.toLocaleDateString('en-us', options);
+
+async function getFruitData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.fruit);
+    displayFruit(data.fruit);
+  }
+  
+  getFruitData(url);
+  
+  const displayFruit = (fruit) => {
+    const cards = document.querySelector('div.cards');
+    
+    fruit.forEach((fruit) => {
+      let card = document.createElement('section');
+      let genus = document.createElement('p');
+      let name = document.createElement('p');
+      let id = document.createElement('p');
+      let family = document.createElement('p');
+      let order = document.createElement('p');
+      let nutritions = document.createElement('p');
+  
+      // console.log(fruit)
+      genus.textContent = `Genus: ${fruit.genus}`;
+      name.textContent = `Name: ${fruit.name}`;
+      id.textContent = `ID: ${fruit.id}`;
+      family.textContent = `Family: ${fruit.family}`;
+      order.textContent = `Order: ${fruit.order}`;
+      nutritions.textContent = `Nutritions: ${fruit.nutritions}`;
+      carbohydrates.textContent = `Carbohydrates: ${fruit.carbohydrates}`;
+      protein.textContent = `Protein: ${fruit.protein}`;
+      fat.textContent = `Fat: ${fruit.fat}`;
+      calories.textContent = `Calories: ${fruit.calories}`;
+      sugar.textContent = `Sugar: ${fruit.sugar}`;
+  
+      card.appendChild(genus);
+      card.appendChild(name);
+      card.appendChild(id);
+      card.appendChild(family);
+      card.appendChild(order);
+      card.appendChild(nutritions);
+      card.appendChild(carbohydrates);
+      card.appendChild(protein);
+      card.appendChild(fat);
+      card.appendChild(calories);
+      card.appendChild(sugar);
+  
+      cards.appendChild(card);
+    }) //close of for each loop
+  }
